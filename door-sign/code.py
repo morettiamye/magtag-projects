e0a2be344838657db3817ec279c98a1e76592827
morettiamye <morettiamye@protonmail.com>
2021-10-07 10:53:26 -0500

    Its alive! Put button handling in function, deep sleep calls function to wake up.

--- door-sign/code.py
+++ door-sign/code.py
@@ -13,43 +13,33 @@ magtag.peripherals.neopixel_disable = True
 
 mid_x = magtag.graphics.display.width // 2 - 1
 magtag.add_text(
-    text_font="opensans-36.bdf",
+    text_font="cousinebold-36.bdf",
     text_position=(mid_x, 20),
     text_anchor_point=(0.5, 0),
     is_data=False,)
 magtag.set_text("Amy Moretti")
 
-magtag.add_text(
-    text_font="opensans-18.bdf",
-    text_wrap=30,
-    text_maxlen=160,
-    text_position=(
-        5,
-        (magtag.graphics.display.height // 2) + 20,
-    ),
-    line_spacing=0.75
-)
+#wakeup = magtag.peripherals.button_a_pressed
 
-magtag.set_text("Hello!", index=1)
+def wakeup():
 
-button = (board.BUTTON_A)  # pick any two
-wakeup = [alarm.pin.PinAlarm(pin=button, value=False, pull=True)]
+    time_span = 0.01;
+    total_sleep_time = 15;
+    time_elapsed = 0;
 
-time_span = 0.01;
-total_sleep_time = 15;
-time_elapsed = 0;
-
-while time_elapsed < total_sleep_time:
-    if magtag.peripherals.button_b_pressed:
-        magtag.set_text("Please knock", index=1)
-    elif magtag.peripherals.button_c_pressed:
-        magtag.set_text("In a meeting, slack me", index=1)
-    elif magtag.peripherals.button_d_pressed:
-        magtag.set_text("Remote today", index=1)
+    while time_elapsed < total_sleep_time:
+        if magtag.peripherals.button_a_pressed:
+            magtag.set_text("BRB!")
+        elif magtag.peripherals.button_b_pressed:
+            magtag.set_text("Please knock")
+        elif magtag.peripherals.button_c_pressed:
+            magtag.set_text("In a meeting,\n slack me")
+        elif magtag.peripherals.button_d_pressed:
+            magtag.set_text("Remote today")
 
     time_elapsed = time_elapsed + time_span
     time.sleep(time_span)
 
-alarm.exit_and_deep_sleep_until_alarms(wakeup)
+magtag.exit_and_deep_sleep(wakeup())
 
 
