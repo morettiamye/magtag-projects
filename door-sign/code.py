7b219cbc3ae8ce0c574bf68e9637165a48bb4d7e
morettiamye <morettiamye@protonmail.com>
2021-10-13 11:28:32 -0500

    Working MagTag Doorsign code

--- door-sign/code.py
+++ door-sign/code.py
@@ -5,13 +5,15 @@ import digitalio
 import neopixel
 import time
 
+pin_alarm = alarm.pin.PinAlarm(pin=board.D11, value=False, pull=True)
+
 magtag = MagTag()
 graphics = magtag.graphics
 peripherals = magtag.peripherals
 
-magtag.peripherals.neopixel_disable = True
+peripherals.neopixel_disable = True
 
-mid_x = magtag.graphics.display.width // 2 - 1
+mid_x = graphics.display.width // 2 - 1
 magtag.add_text(
     text_font="cousinebold-36.bdf",
     text_position=(mid_x, 20),
@@ -19,27 +21,23 @@ magtag.add_text(
     is_data=False,)
 magtag.set_text("Amy Moretti")
 
-#wakeup = magtag.peripherals.button_a_pressed
 
-def wakeup():
 
-    time_span = 0.01;
-    total_sleep_time = 15;
-    time_elapsed = 0;
+time_span = 0.01;
+total_sleep_time = 15;
+time_elapsed = 0;
 
-    while time_elapsed < total_sleep_time:
-        if magtag.peripherals.button_a_pressed:
-            magtag.set_text("BRB!")
-        elif magtag.peripherals.button_b_pressed:
-            magtag.set_text("Please knock")
-        elif magtag.peripherals.button_c_pressed:
-            magtag.set_text("In a meeting,\n slack me")
-        elif magtag.peripherals.button_d_pressed:
-            magtag.set_text("Remote today")
+while time_elapsed < total_sleep_time:
+    if peripherals.button_a_pressed:
+        magtag.set_text("Please knock")
+    elif peripherals.button_b_pressed:
+        magtag.set_text("In a meeting,\n slack me")
+    elif peripherals.button_c_pressed:
+        magtag.set_text("Remote today")
+    elif peripherals.button_d_pressed:
+        magtag.set_text("BRB")
 
     time_elapsed = time_elapsed + time_span
     time.sleep(time_span)
 
-magtag.exit_and_deep_sleep(wakeup())
-
-
+alarm.exit_and_deep_sleep_until_alarms(pin_alarm)
